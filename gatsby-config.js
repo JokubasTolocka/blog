9725e751a8cbf8e47ba7235df994088a0e3b997e
Jokūbas Toločka <jokubastolocka@pop-os.localdomain>
2021-01-20 15:24:22 +0200

    change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "markdown-pages",
-        path: `${__dirname}/content/blog/`,
+        path: `${__dirname}/content`,
       },
       __key: "pages",
     },
