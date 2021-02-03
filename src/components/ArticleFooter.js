2754ef6e16cb63a920af6921195e71c4d25a948c
JokubasTolocka <jokubas.tj@gmail.com>
2021-02-03 18:46:37 +0200

    wip

--- src/components/ArticleFooter.js
+++ src/components/ArticleFooter.js
@@ -35,20 +35,20 @@ const ArticleFooter = ({ category }) => {
   // const articles = data.allMarkdownRemark.edges;
 
   return (
-    // <div className={styles.root}>
-    //   <div className={styles.content}>
-    //     <h4 className={styles.title}>Other articles</h4>
-    //     <div className={styles.articles}>
-    //       {articles.map((article, index) => (
-    //         <ArticleBlockFooter
-    //           key={index}
-    //           article={article}
-    //           path={data.allMarkdownRemark.nodes[index].fields.path}
-    //         />
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
+    <div className={styles.root}>
+      {/* <div className={styles.content}>
+        <h4 className={styles.title}>Other articles</h4>
+        <div className={styles.articles}>
+          {articles.map((article, index) => (
+            <ArticleBlockFooter
+              key={index}
+              article={article}
+              path={data.allMarkdownRemark.nodes[index].fields.path}
+            />
+          ))}
+        </div>
+      </div> */}
+    </div>
   );
 };
 
