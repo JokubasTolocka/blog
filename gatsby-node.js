1331a7f1faa4a1c8d99d36778c42fee66f6c1501
JokubasTolocka <jokubas.tj@gmail.com>
2021-02-03 18:42:57 +0200

    wip

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,59 @@
-const _ = require("lodash");
-const path = require("path");
-const { createFilePath } = require("gatsby-source-filesystem");
+// const _ = require("lodash");
+// const path = require("path");
+// const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = async ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  // const result = await graphql(`
-  //   {
-  //     allMarkdownRemark {
-  //       nodes {
-  //         id
-  //         fields {
-  //           path
-  //           slug
-  //         }
-  //         frontmatter {
-  //           category
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+//   // const result = await graphql(`
+//   //   {
+//   //     allMarkdownRemark {
+//   //       nodes {
+//   //         id
+//   //         fields {
+//   //           path
+//   //           slug
+//   //         }
+//   //         frontmatter {
+//   //           category
+//   //         }
+//   //       }
+//   //     }
+//   //   }
+//   // `);
 
-  if (result.errors) {
-    result.errors.forEach((error) => console.error(error.toString()));
-    throw result.errors;
-  }
+//   if (result.errors) {
+//     result.errors.forEach((error) => console.error(error.toString()));
+//     throw result.errors;
+//   }
 
-  const posts = result.data.allMarkdownRemark.nodes;
+//   const posts = result.data.allMarkdownRemark.nodes;
 
-  posts.forEach((post) => {
-    createPage({
-      path: post.fields.path,
-      component: path.resolve(`./src/templates/blog.js`),
-      context: {
-        id: post.id,
-      },
-    });
-  });
-};
+//   posts.forEach((post) => {
+//     createPage({
+//       path: post.fields.path,
+//       component: path.resolve(`./src/templates/blog.js`),
+//       context: {
+//         id: post.id,
+//       },
+//     });
+//   });
+// };
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions;
 
-  if (node.internal.type === "MarkdownRemark") {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: "slug",
-      node,
-      value: value.match(/\/([^\/]+?)\/$/)[1],
-    });
-    createNodeField({
-      name: "path",
-      node,
-      value: value.replace(/\/$/, ""),
-    });
-  }
-};
+//   if (node.internal.type === "MarkdownRemark") {
+//     const value = createFilePath({ node, getNode });
+//     createNodeField({
+//       name: "slug",
+//       node,
+//       value: value.match(/\/([^\/]+?)\/$/)[1],
+//     });
+//     createNodeField({
+//       name: "path",
+//       node,
+//       value: value.replace(/\/$/, ""),
+//     });
+//   }
+// };
